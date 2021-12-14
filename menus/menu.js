09a92497f9a948be32407945cddb36eec33bcd59
blabla <blablablabla@gmail.com>
2021-12-14 12:31:29 -0300

    blabla

--- menus/menu.js
+++ menus/menu.js
@@ -2,7 +2,7 @@ const menu = (prefix, hordat, numerodn, NickDono) => {
 return `​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​
 ╭⎆──━──⊱⛦⊰──━──╮
 │
-│⎘⃯⃗➢ seja bem vindo(a) 
+│⎘⃯⃗➢ Seja bem vindo(a) 
 │⎘⃯⃗➢ ao menu de comandos
 │
 ├⎆──━──⊱⛦⊰──━──╮
@@ -14,112 +14,100 @@ return `​​​​​​​​​​​​​​​​​​​​​​​​
 │  ✰•IMPORTANTE•✰
 ├⎆──━──⊱⛦⊰──━──╯
 │
-│⎘⃯⃗➢ ${prefix}infopalavrão
-│⎘⃯⃗➢ ${prefix}infolistanegra
-│⎘⃯⃗➢ ${prefix}infobancarac
-│⎘⃯⃗➢ ${prefix}infovotação
-│⎘⃯⃗➢ ${prefix}infocontador
-│⎘⃯⃗➢ ${prefix}infosorteio
-│⎘⃯⃗➢ ${prefix}infobot
-│⎘⃯⃗➢ ${prefix}idiomas
-│⎘⃯⃗➢ ${prefix}bug (questione)
-│⎘⃯⃗➢ ${prefix}avalie (o quao bom)
-│⎘⃯⃗➢ ${prefix}sugestao (dica)
+│⎘⃯⃗➢ ${prefix}Infopalavrão
+│⎘⃯⃗➢ ${prefix}Infolistanegra
+│⎘⃯⃗➢ ${prefix}Infobancarac
+│⎘⃯⃗➢ ${prefix}Infovotação
+│⎘⃯⃗➢ ${prefix}Infocontador
+│⎘⃯⃗➢ ${prefix}Infosorteio
+│⎘⃯⃗➢ ${prefix}Infobot
+│⎘⃯⃗➢ ${prefix}Idiomas
+│⎘⃯⃗➢ ${prefix}Bug (questione)
+│⎘⃯⃗➢ ${prefix}Avalie (o quao bom)
+│⎘⃯⃗➢ ${prefix}Sugestao (dica)
 │
 ├⎆──━──⊱⛦⊰──━──╮
 │  ✰•MENU-DE-CMD •✰
 ├⎆──━──⊱⛦⊰──━──╯
 │
-│⎘⃯⃗➢ ${prefix}menudono
-│⎘⃯⃗➢ ${prefix}efeitosimg
-│⎘⃯⃗➢ ${prefix}menuadm
-│⎘⃯⃗➢ ${prefix}menupremium
-│⎘⃯⃗➢ ${prefix}logos
-│⎘⃯⃗➢ ${prefix}brincadeiras
-│⎘⃯⃗➢ ${prefix}alteradores
+│⎘⃯⃗➢ ${prefix}Menudono
+│⎘⃯⃗➢ ${prefix}Efeitosimg
+│⎘⃯⃗➢ ${prefix}Menuadm
+│⎘⃯⃗➢ ${prefix}Menupremium
+│⎘⃯⃗➢ ${prefix}Logos
+│⎘⃯⃗➢ ${prefix}Brincadeiras
+│⎘⃯⃗➢ ${prefix}Alteradores
+│⎘⃯⃗➢ ${prefix}Herokulogos
 │
 ├⎆──━──⊱⛦⊰──━──╮
 │ ✰•PESQUISA/BAIXAR•✰
 ├⎆──━──⊱⛦⊰──━──╯
 │ 
-│⎘⃯⃗➢ ${prefix}play
-│⎘⃯⃗➢ ${prefix}play2
-│⎘⃯⃗➢ ${prefix}play3
-│⎘⃯⃗➢ ${prefix}play4
-│⎘⃯⃗➢ ${prefix}playmp4
-│⎘⃯⃗➢ ${prefix}ytsearch
-│⎘⃯⃗➢ ${prefix}ytmp4
-│⎘⃯⃗➢ ${prefix}ytmp3
-│⎘⃯⃗➢ ${prefix}facemp4
-│⎘⃯⃗➢ ${prefix}playstore
-│⎘⃯⃗➢ ${prefix}mediafire (url)
-│⎘⃯⃗➢ ${prefix}imgpralink
-│⎘⃯⃗➢ ${prefix}videopralink
+│⎘⃯⃗➢ ${prefix}Play (NOME)
+│⎘⃯⃗➢ ${prefix}Play2 (NOME)
+│⎘⃯⃗➢ ${prefix}Play3 (NOME)
+│⎘⃯⃗➢ ${prefix}Play4 (NOME)
+│⎘⃯⃗➢ ${prefix}Playmp4 (NOME)
+│⎘⃯⃗➢ ${prefix}Ytsearch (NOME)
+│⎘⃯⃗➢ ${prefix}Ytmp4 (LINK)
+│⎘⃯⃗➢ ${prefix}Ytmp3 (LINK)
+│⎘⃯⃗➢ ${prefix}Imgpralink (MARCAR)
+│⎘⃯⃗➢ ${prefix}Videopralink (MARCAR-V)
 │ 
 ├⎆──━──⊱⛦⊰──━──╮
 │  ✰•INFORMAÇÕES•✰
 ├⎆──━──⊱⛦⊰──━──╯
 │
-│⎘⃯⃗➢ ${prefix}ping (velo)
-│⎘⃯⃗➢ ${prefix}bateria
-│⎘⃯⃗➢ ${prefix}gitdobot
-│⎘⃯⃗➢ ${prefix}atividade
-│⎘⃯⃗➢ ${prefix}rankativo
-│⎘⃯⃗➢ ${prefix}checkativo @marca
-│⎘⃯⃗➢ ${prefix}tabela (tabela-letras)
-│⎘⃯⃗➢ ${prefix}ranklevel (de todos)
+│⎘⃯⃗➢ ${prefix}Ping (VELO-TEMPO)
+│⎘⃯⃗➢ ${prefix}Bateria
+│⎘⃯⃗➢ ${prefix}Gitdobot
+│⎘⃯⃗➢ ${prefix}Atividade
+│⎘⃯⃗➢ ${prefix}Rankativo
+│⎘⃯⃗➢ ${prefix}Checkativo (@MARCAR)
+│⎘⃯⃗➢ ${prefix}Ranklevel (DO-GRUPO)
 │
 ├⎆──━──⊱⛦⊰──━──╮
 │    ✰•JOGOS•✰
 ├⎆──━──⊱⛦⊰──━──╯
 │
-│⎘⃯⃗➢ ${prefix}ppt (papel-tesoura) 
-│⎘⃯⃗➢ ${prefix}jogodavelha @marca
-│⎘⃯⃗➢ ${prefix}ttt (jogo da velha) 
-│⎘⃯⃗➢ ${prefix}cassino
-│⎘⃯⃗➢ ${prefix}anagrama
+│⎘⃯⃗➢ ${prefix}Ppt (PEDRA-PAPEL-TESOURA) 
+│⎘⃯⃗➢ ${prefix}Jogodavelha (@MARCAR)
+│⎘⃯⃗➢ ${prefix}Ttt (JOGO-DA-VELHA) 
+│⎘⃯⃗➢ ${prefix}Cassino
+│⎘⃯⃗➢ ${prefix}Anagrama 1 / 0
 │
 ├⎆──━──⊱⛦⊰──━──╮
 │  ✰•FIGURINHAS•✰
 ├⎆──━──⊱⛦⊰──━──╯
 │ 
-│⎘⃯⃗➢ ${prefix}fsticker
-│⎘⃯⃗➢ ${prefix}sticker
-│⎘⃯⃗➢ ${prefix}togif
-│⎘⃯⃗➢ ${prefix}toimg
+│⎘⃯⃗➢ ${prefix}Fsticker
+│⎘⃯⃗➢ ${prefix}Sticker
+│⎘⃯⃗➢ ${prefix}Togif
+│⎘⃯⃗➢ ${prefix}Toimg
 │
 ├⎆──━──⊱⛦⊰──━──╮
 │✰•ATTPS/CMD-DE-TXT•✰
 ├⎆──━──⊱⛦⊰──━──╯
 │
-│⎘⃯⃗➢ ${prefix}attp  (txt)
-│⎘⃯⃗➢ ${prefix}ttp (txt)
-│⎘⃯⃗➢ ${prefix}attp2 (txt)
-│⎘⃯⃗➢ ${prefix}attp3 (txt)
-│⎘⃯⃗➢ ${prefix}attp4 (txt)
-│⎘⃯⃗➢ ${prefix}attp5 (txt)
-│⎘⃯⃗➢ ${prefix}attp6 (txt)
-│⎘⃯⃗➢ ${prefix}ttm   (txt)
-│⎘⃯⃗➢ ${prefix}ttm2  (txt)
-│⎘⃯⃗➢ ${prefix}ttm3  (txt)
-│⎘⃯⃗➢ ${prefix}ttm4  (txt)
+│⎘⃯⃗➢ ${prefix}Attp  (txt)
+│⎘⃯⃗➢ ${prefix}Ttp (txt)
 │
 ├⎆──━──⊱⛦⊰──━──╮
 │  ✰•CMD-BÁSICOS•✰
 ├⎆──━──⊱⛦⊰──━──╯
 │ 
-│⎘⃯⃗➢ ${prefix}gtts (linguagem)
-│⎘⃯⃗➢ ${prefix}tagme
-│⎘⃯⃗➢ ${prefix}emoji
-│⎘⃯⃗➢ ${prefix}tabela
-│⎘⃯⃗➢ ${prefix}conselhobiblico
-│⎘⃯⃗➢ ${prefix}simi (fale algo) 
-│⎘⃯⃗➢ ${prefix}perfil
-│⎘⃯⃗➢ ${prefix}wallpaperanime
-│⎘⃯⃗➢ ${prefix}tomp3 
-│⎘⃯⃗➢ ${prefix}frases
-│⎘⃯⃗➢ ${prefix}fazernick (nick)
-│⎘⃯⃗➢ ${prefix}bot
+│⎘⃯⃗➢ ${prefix}Gtts (linguagem)
+│⎘⃯⃗➢ ${prefix}Tagme
+│⎘⃯⃗➢ ${prefix}Emoji
+│⎘⃯⃗➢ ${prefix}Tabela (LETRAS/SÍMBOLOS)
+│⎘⃯⃗➢ ${prefix}Conselhobiblico
+│⎘⃯⃗➢ ${prefix}Simi (fale algo) 
+│⎘⃯⃗➢ ${prefix}Perfil
+│⎘⃯⃗➢ ${prefix}Wallpaperanime
+│⎘⃯⃗➢ ${prefix}Tomp3 
+│⎘⃯⃗➢ ${prefix}Frases
+│⎘⃯⃗➢ ${prefix}Fazernick (nick)
+│⎘⃯⃗➢ ${prefix}Bot
 │
 ├⎆──━──⊱⛦⊰──━──╯
 │
