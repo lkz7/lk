6dc527e7bd14b9449d2c831abd4a903f96f5aee0
blabla <blablablabla@gmail.com>
2021-12-24 06:34:35 -0300

    blabla

--- menus/menu.js
+++ menus/menu.js
@@ -1,4 +1,4 @@
-const menu = (prefix, hora, data, numerodn, NickDono) => {
+const menu = (prefix, hora, datt, numerodn, NickDono) => {
   
 // NÃO APAGUE ESSE ${NickDono} nem 
 //${numerodn} nem ${NomeDoBot} nem ${prefix} só se quiser apagar completo, não coloque nada ${dentro assim} ISSO SÃO DEFINIÇÕES QUE ESTÁ PUXANDO DO settings.json, da pasta dono, só pode altera a base de tudo, menos as definições, só se quiser apagar a definição completa.  
@@ -17,7 +17,7 @@ return `​​​​​​​​​​​​​​​​​​​​​​​​
 ║
 ║♡ۣۜۜ፝͜͜͡͡✿➤ Prefixo: ⟮•  ${prefix}  •⟯
 ║♡ۣۜۜ፝͜͜͡͡✿➤ Hora : ${hora} 
-║♡ۣۜۜ፝͜͜͡͡✿➤ Data : ${data}
+║♡ۣۜۜ፝͜͜͡͡✿➤ Data : ${datt}
 ║
 ╠┅═┅══ ᐃᐁᐃ ══┅═┅═᳀
 ║       ⬕•IMPORTANTE•⬕
