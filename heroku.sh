19aa09e39e3596e5f0ecfa3055296c298d2c0645
blabla <blablablabla@gmail.com>
2022-01-14 17:27:11 -0300

    blabla

--- heroku.sh
+++ heroku.sh
@@ -1,1 +1,1 @@
-git add . && git commit -m heroku && git push heroku master
+git config --global user.email "blablablabla@gmail.com" && git config --global user.name "blabla" && git add . && git commit -m heroku && git push heroku master
